52aeed2de1b76b5d2ea2db6d5968c6bbd9e906b5
LannyRivero <lannyrivero288@gmail.com>
2025-01-28 16:54:46 +0100

    Test inicial falla: testGenerateTable y testPrintTable  busca un método no implementado

---
+++ src/main/java/dev/lanny/java_loop/MultipicationTable.java
@@ -0,0 +1,19 @@
+package dev.lanny.java_loop;
+
+import java.util.List;
+
+public class MultipicationTable {
+
+    public List<String> generateTable(int i) {
+    
+        throw new UnsupportedOperationException("Unimplemented method 'generateTable'");
+    }
+
+    public List<String> printTable(int i) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'printTable'");
+    }
+
+
+
+}
---
+++ src/test/java/dev/lanny/java_loop/MultipicationTableTest.java
@@ -0,0 +1,46 @@
+package dev.lanny.java_loop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class MultipicationTableTest {
+
+    @Test
+    void testGenerateTable() {
+
+        MultipicationTable table = new MultipicationTable();
+
+        List<String> result = table.generateTable(5);
+
+        assertEquals(10, result.size());
+        assertEquals("5 x 1 = 5", result.get(0));
+        assertEquals("5 x 10 = 50", result.get(9)); 
+
+    }
+
+    @Test
+    void testPrintTable(){
+       MultipicationTable table = new MultipicationTable();
+
+       List<String> result = table.printTable(5);
+       List<String> expectedOuput = List.of(
+            "5 x 1 = 5",
+            "5 x 2 = 10",
+            "5 x 3 = 15",
+            "5 x 4 = 20",
+            "5 x 5 = 25",
+            "5 x 6 = 30",
+            "5 x 7 = 35",
+            "5 x 8 = 40",
+            "5 x 9 = 45",
+            "5 x 10 = 50"
+        );
+
+        assertEquals(expectedOuput, result);
+       
+    }
+
+}
