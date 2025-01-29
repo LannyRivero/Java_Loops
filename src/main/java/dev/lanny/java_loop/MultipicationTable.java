c69d67b0337f2be6a9f8e8c3a2df2b09918fc7c0
LannyRivero <lannyrivero288@gmail.com>
2025-01-29 21:19:56 +0100

    Implementación de generateTable y printTable, tests exitosos

--- src/main/java/dev/lanny/java_loop/MultipicationTable.java
+++ src/main/java/dev/lanny/java_loop/MultipicationTable.java
@@ -1,19 +1,29 @@
 package dev.lanny.java_loop;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class MultipicationTable {
 
-    public List<String> generateTable(int i) {
-    
-        throw new UnsupportedOperationException("Unimplemented method 'generateTable'");
+    public List<String> generateTable(int n) {
+        List<String> table = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            table.add(n + " x " + i + " = " + n * i);
+        }
+        return table;
     }
 
-    public List<String> printTable(int i) {
+
+    public List<String> printTable(int n) {
         
-        throw new UnsupportedOperationException("Unimplemented method 'printTable'");
+        List<String> table = generateTable(n);
+
+        for (String row : table) {
+
+            System.out.println(row);
+        }
+        return table;
     }
-
-
 
 }
+
